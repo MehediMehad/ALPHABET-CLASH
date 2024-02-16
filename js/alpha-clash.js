87eac2bee04036a97184ca8ea74c8180a5e0be63
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-02-16 20:50:59 +0600

    Score updeted and life updeted

--- js/alpha-clash.js
+++ js/alpha-clash.js
@@ -1,10 +1,31 @@
+function handleKeyboardButtonPrees(event) {
+    const playerPressed = event.key;
+    const caurrentAlphabetElement = document.getElementById('current-alphabet')
+    const caurrentAlphabet = caurrentAlphabetElement.innerText;
+    const expectedAlphabet = caurrentAlphabet.toLocaleLowerCase();
+    console.log(playerPressed, expectedAlphabet);
+
+    if (playerPressed === expectedAlphabet) {
+        console.log('Gaot a poin');
+        continueGame();
+        removeBgColorById(expectedAlphabet)
+    }
+    else{
+        console.log('die');
+    }
+ }
+
+document.addEventListener('keyup', handleKeyboardButtonPrees)
+
 function continueGame() {
     const alphabet = getARandomAlphabet();
     const caurrentAlphabetElement =document.getElementById('current-alphabet')
     caurrentAlphabetElement.innerText = alphabet;
+    setBgColorById(alphabet)
+ 
 }
 function play() {
     hideElementById('home-screen')
     showElementById('play-ground')
-    continueGame()
+    continueGame();
 }
--- js/utility.js
+++ js/utility.js
@@ -9,12 +9,19 @@ function showElementById(elementId) {
     element.classList.remove('hidden')
 }
 
+function setBgColorById(elementId) {
+    const element = document.getElementById(elementId)
+    element.classList.add('bg-orange-500');
+}
+function removeBgColorById(elementId) {
+    const element = document.getElementById(elementId)
+    element.classList.remove('bg-orange-500');
+}
  function getARandomAlphabet() {
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
     const alphabets = alphabetString.split('')
     const rendomNumber = Math.random() *25;
     const index = Math.round(rendomNumber)
-
-    const alphabet = alphabets[index]
+    const alphabet = alphabets[index]; 
     return alphabet;
  } 
